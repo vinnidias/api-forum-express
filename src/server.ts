3ff30c10ab1dc3b40bcc3a11c593158bb413cdb0
vinnidias <vinicius.3991@gmail.com>
2023-04-21 13:38:02 -0300

    creating error use on server

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,30 @@
-import express from 'express';
-import { routes } from './routes';
+import "express-async-errors";
+import express, { Request, Response, NextFunction } from "express";
+import { routes } from "./routes";
+import { AppError } from "./errors/AppError";
 
 const app = express();
 
 const port = process.env.PORT || 3330;
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(routes);
 
-app.listen(port, ()=> console.log("server is running on port", port))
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: "error",
+        message: err.message,
+      });
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error - ${err.message}`,
+    });
+  }
+);
+
+app.listen(port, () => console.log("server is running on port", port));
