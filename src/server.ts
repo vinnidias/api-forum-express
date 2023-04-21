5774f97bfb2f90082649be51e841871de6ebbd79
vinnidias <vinicius.3991@gmail.com>
2023-04-21 13:02:06 -0300

    passing routes to server

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 import express from 'express';
+import { routes } from './routes';
 
 const app = express();
 
 const port = process.env.PORT || 3330;
+
+app.use(express.json())
+
+app.use(routes);
 
 app.listen(port, ()=> console.log("server is running on port", port))
