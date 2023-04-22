6c8b32a8674ab636dbf19b43d1c8c0aa3de74beb
vinnidias <vinicius.3991@gmail.com>
2023-04-22 19:25:45 -0300

    passing discussion routes to main route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { userRoutes } from "./user.routes";
+import { discussionRoutes } from "./discussion.routes";
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
+routes.use("/discussion", discussionRoutes);
 
 export { routes };
