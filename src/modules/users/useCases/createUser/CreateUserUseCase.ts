808b9d50db0dc3b40376c31de5cd67edc4aadbc8
vinnidias <vinicius.3991@gmail.com>
2023-04-22 18:18:44 -0300

    create user useCase created

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -1,4 +1,4 @@
-import { AppError } from "../../../../errors/AppError";
+ import { AppError } from "../../../../errors/AppError";
 import { prisma } from "../../../../prisma/client";
 import { CreateUserDTO } from "../../dtos/CreateUserDTO";
 import { User } from "@prisma/client";
